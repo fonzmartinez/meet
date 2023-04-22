64686ae76afc2ef3f7f583c42bbbffbf354746e3
fonzmartinez <119453742+fonzmartinez@users.noreply.github.com>
2023-04-22 10:52:01 -0500

    New Alert Added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 import CitySearch from "./CitySearch";
 import EventList from "./EventList";
 import NumberOfEvents from "./NumberOfEvents";
-import { getEvents, extractLocations } from "./api";
+import { WarningAlert } from "./Alert";
+import WelcomeScreen from "./WelcomeScreen";
+import { getEvents, extractLocations, checkToken, getAccessToken } from "./api";
 import "./nprogress.css";
 
 
@@ -14,6 +16,149 @@ class App extends Component {
     locations: [],
     eventCount: 32,
     selectedCity: null,
+    showWelcomeScreen: undefined
+  };
+
+  updateEvents = (location, eventCount) => {
+    if (!eventCount) {
+      getEvents().then((events) => {
+        const locationEvents = (location === 'all') ?
+          events :
+          events.filter((event) => event.location === location);
+        const NumEvents = locationEvents.slice(0, this.state.eventCount);
+        this.setState({
+          events: NumEvents,
+          selectedCity: location,
+        });
+      });
+    }
+    else if (eventCount && !location) {
+      getEvents().then((events) => {
+        const locationEvents = events.filter((event) =>
+          this.state.locations.includes(event.location)
+        );
+        const NumEvents = locationEvents.slice(0, eventCount);
+        this.setState({
+          events: NumEvents,
+          eventCount: eventCount,
+        });
+      });
+    }
+    else if (this.state.selectedCity === "all") {
+      getEvents().then((events) => {
+        const locationEvents = events;
+        const NumEvents = locationEvents.slice(0, eventCount);
+        this.setState({
+          events: NumEvents,
+          eventCount: eventCount,
+        });
+      });
+    }
+    else {
+      getEvents().then((events) => {
+        const locationEvents = (this.state.locations === "all") ?
+          events :
+          events.filter((event) => this.state.selectedCity === event.location);
+        const NumEvents = locationEvents.slice(0, eventCount);
+        this.setState({
+          events: NumEvents,
+          eventCount: eventCount,
+        });
+      });
+    }
+  };
+
+  async componentDidMount() {
+    this.mounted = true;
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false :
+      true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+      });
+    }
+  }
+
+
+  /*
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        const NumEvents = events.slice(0, this.state.eventCount);
+        this.setState({
+          events: NumEvents,
+          locations: extractLocations(events),
+        });
+      }
+    });
+  }
+  */
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
+  render() {
+    if (this.state.showWelcomeScreen === undefined) return <div
+      className="App" />
+    return (
+      <div className="App">
+        {/* Other components such as CitySearch, EventList,...etc */}
+        <br></br>
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+        />
+        <br></br>
+        <NumberOfEvents
+          selectedCity={this.state.selectedCity}
+          number={this.state.eventCount}
+          updateEvents={this.updateEvents}
+        />
+        {!navigator.onLine &&
+          <WarningAlert text='You are currently offline and display has been loaded from cache and may not be up to date.' />
+        }
+        <br></br>
+        <EventList events={this.state.events} />
+        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
+          getAccessToken={() => { getAccessToken() }} />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+
+/*
+import React, { Component } from "react";
+import "./App.css";
+import CitySearch from "./CitySearch";
+import EventList from "./EventList";
+import NumberOfEvents from "./NumberOfEvents";
+import { WarningAlert } from "./Alert";
+import WelcomeScreen from "./WelcomeScreen";
+import { getEvents, extractLocations, checkToken, getAccessToken } from "./api";
+import "./nprogress.css";
+
+
+
+class App extends Component {
+  state = {
+    events: [],
+    locations: [],
+    eventCount: 32,
+    selectedCity: null,
+    showWelcomeScreen: undefined
   };
 
   updateEvents = (location, eventCount) => {
@@ -96,6 +241,9 @@ class App extends Component {
           number={this.state.eventCount}
           updateEvents={this.updateEvents}
         />
+        {!navigator.onLine &&
+          <WarningAlert text='You are currently offline and display has been loaded from cache and may not be up to date.' />
+        }
         <br></br>
         <EventList events={this.state.events} />
       </div>
@@ -104,3 +252,4 @@ class App extends Component {
 }
 
 export default App;
+*/
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+//import * as serviceWorker from './service-worker';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import * as atatus from 'atatus-spa';
@@ -19,7 +20,9 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+//serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
+//serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
